96588a03a2b0b30e89c315ce32eb181623ab503d
manishsaraan <manish021js@gmail.com>
2017-12-29 22:40:31 -0800

    base component added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// Set up your application entry point here...
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import routes from './routes';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/common/HomePage'
+render(
+  <Router  routes={routes} />,
+  document.getElementById('app')
+);
